94d739a0811b8fe58bfc4a2403e37dce8202d4ff
addegbenga <addegbenga@gmail.com>
2021-09-30 13:53:28 -0400

    refactored modal

--- src/dashboard/admin.js
+++ src/dashboard/admin.js
@@ -6,6 +6,7 @@ import { Dialog } from "@headlessui/react";
 import SocialModal from "../modals/SocialModal";
 import OtherLinkModal from "../modals/OtherLinkModal";
 import AvatarModal from "../modals/AvatarModal";
+import AboutModal from "../modals/AboutModal";
 export default function Admin() {
   const [isOpen, setIsOpen] = useState(false);
   const [openSocialModal, setSocialModal] = useState(false);
@@ -58,93 +59,14 @@ export default function Admin() {
         isOpen={openOtherLinkModal}
         closeModal={handleOtherLinkModalClose}
       />
+      <AboutModal isOpen={isOpen} closeModal={closeModal} />
       <Navbar />
       <div className=" flex flex-col items-center text-center p-4 py-16 ">
-        <>
-          <Transition appear show={isOpen} as={Fragment}>
-            <Dialog
-              as="div"
-              className="fixed inset-0 z-10 overflow-y-auto"
-              onClose={closeModal}
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-              <div className="min-h-screen px-4 text-center">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Dialog.Overlay className="fixed inset-0" />
-                </Transition.Child>
-
-                {/* This element is to trick the browser into centering the modal contents. */}
-                <span
-                  className="inline-block h-screen align-middle"
-                  aria-hidden="true"
-                >
-                  &#8203;
-                </span>
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0 scale-95"
-                  enterTo="opacity-100 scale-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100 scale-100"
-                  leaveTo="opacity-0 scale-95"
-                >
-                  <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-lg">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 pb-4 text-gray-900"
-                    >
-                      Edit your Profile
-                    </Dialog.Title>
-
-                    <div className="grid mb-4">
-                      <label className="text-sm text-left text-semibold">
-                        Full name
-                      </label>
-                      <input
-                        placeholder="John doe"
-                        className="border-0 bg-gray-100 text-sm p-4   focus:ring-4 rounded mt-2"
-                        type="text"
-                      />
-                    </div>
-                    <div className="grid mb-4">
-                      <label className="text-sm text-left text-semibold">
-                        Enter bio
-                      </label>
-                      <textarea
-                        rows="8"
-                        placeholder="Enter your bio"
-                        className="border-0 bg-gray-100 text-sm p-4  focus:ring-4 rounded mt-2"
-                        type="text"
-                      />
-                    </div>
-
-                    <div className="mt-4 flex justify-end">
-                      <button
-                        type="button"
-                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-neutral-normal border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                        onClick={closeModal}
-                      >
-                        Save
-                      </button>
-                    </div>
-                  </div>
-                </Transition.Child>
-              </div>
-            </Dialog>
-          </Transition>
-        </>
-
         <div className="relative">
-          <button className="bg-black h-7 w-7 flex justify-center items-center absolute bg-white text-xs bottom-0 right-0 shadow rounded-full " onClick={handleAvatarModalOpen}>
+          <button
+            className="bg-black h-7 w-7 flex justify-center items-center absolute bg-white text-xs bottom-0 right-0 shadow rounded-full "
+            onClick={handleAvatarModalOpen}
+          >
             {" "}
             <i className="fa fa-edit"></i>
           </button>
