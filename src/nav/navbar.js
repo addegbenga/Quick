749bd42cebbc4cec82e5e8a0d1dede9c970c2122
addegbenga <addegbenga@gmail.com>
2021-10-23 10:09:18 -0400

    desktop

--- src/nav/navbar.js
+++ src/nav/navbar.js
@@ -6,7 +6,7 @@ export default function navbar() {
   return (
     <div className="bg-white shadow-lg md:shadow-none border w-full z-10 fixed w-full top-0">
       <div className=" lg:w-5/6 md:w-5/6 md:m-auto flex lg:m-auto xl:w-5/6 xl:m-auto justify-between items-center p-4 md:p-6 lg:p-6 xl:p-6 ">
-        <img className="w-20 " src={logo} alt="logo" />
+        <img className="w-20 lg:w-32 " src={logo} alt="logo" />
         <div className="flex items-center ">
           <ul className="hidden md:block mr-10  text-sm">
             <a href="#e">Home</a>
