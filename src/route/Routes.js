3364814c3d4cdc40f79bd52e9b7e1d2d7868c3a9
addegbenga <addegbenga@gmail.com>
2021-11-28 14:26:21 -0500

    chnanges

--- src/route/Routes.js
+++ src/route/Routes.js
@@ -9,19 +9,24 @@ import Landing from "../home/Landing";
 
 export default function Routes() {
   return (
-    <div>
-      <Switch>
-        <Route component={Landing} exact={true} path="/" />
-        <Route component={Signin} exact={true} path="/login" />
-        <Route component={Signup} exact={true} path="/register" />
-        <Route
-          component={ForgotPassword}
-          exact={true}
-          path="/forgot-password"
-        />
-        <Route path="/name" exact={true} component={AdminDashboard}/>
-        {/* <ProtectedRoute path="/name" exact={true} component={AdminDashboard} /> */}
-      </Switch>
-    </div>
+    <>
+      <div className="hidden lg:block">
+        Desktop mood cooming soon, switch to mobile view
+      </div>
+      <div className="lg:hidden block">
+        <Switch>
+          <Route component={Landing} exact={true} path="/" />
+          <Route component={Signin} exact={true} path="/login" />
+          <Route component={Signup} exact={true} path="/register" />
+          <Route
+            component={ForgotPassword}
+            exact={true}
+            path="/forgot-password"
+          />
+          <Route path="/name" exact={true} component={AdminDashboard} />
+          {/* <ProtectedRoute path="/name" exact={true} component={AdminDashboard} /> */}
+        </Switch>
+      </div>
+    </>
   );
 }
