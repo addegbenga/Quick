97a09e76186b50e1e7788155b6b4af0501a6e792
addegbenga <addegbenga@gmail.com>
2021-12-31 05:35:29 -0500

    changes

--- src/components/dashboard/PanelNav.js
+++ src/components/dashboard/PanelNav.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import {useSelector} from "react-redux"
 export default function PanelNav() {
+  const user = useSelector((state)=> state.auth.user)
   return (
     <div className="lg:h-16 h-14 flex items-center justify-between px-6 border-b  bg-white ">
       <div className="flex text-sm">
         <h1 className="font-semibold mr-1">My Linktree:</h1>
-        <a href="#div">https://linktree/drey</a>
+        <a href="#div">{`https://linktree/${user.username}`}</a>
       </div>
       <button className="border py-1 px-3 rounded text-base text-opacity-60 text-black">
         Share
