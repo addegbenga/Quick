df29a0111e58116e48023cf5aaecf6bc35ca7cb1
Adeyemi <addegbenga@gmail.com>
2022-06-06 01:17:03 +0100

    added gif to landing page

--- src/pages/landing/Home.js
+++ src/pages/landing/Home.js
@@ -72,32 +72,32 @@ export default function Home() {
       <Navbar />
       {/* <Login isOpen={loginModalOpen} setIsOpen={setLoginModalOpen} /> */}
       <div
-        className=" relative  flex pt-36 lg:pt-48 2xl:pt-80 items-center flex-col bg-bgPrimary  items-center"
+        className="relative flex flex-col items-center pt-36 lg:pt-48 2xl:pt-80 bg-bgPrimary"
         style={{ height: "100%", minHeight: "70vh" }}
       >
         <img
-          className="absolute top-26 w-56 -left-10 "
+          className="absolute w-56 top-26 -left-10 "
           src="/assets/qwikkerasset17.png"
           alt="asset"
         />
-        <h1 className="  md:text-5xl px-5 text-4xl lg:text-6xl 2xl:w-3/4   lg:px-20 xl:px-64 md:px-10   relative z-20 text-textPrimaryColor font-black text-center">
+        <h1 className="relative z-20 px-5 text-4xl font-black text-center md:text-5xl lg:text-6xl 2xl:w-3/4 lg:px-20 xl:px-64 md:px-10 text-textPrimaryColor">
           Modern platform to have your social links.
         </h1>
 
-        <button className="text-white 2xl:text-lg relative z-20 bg-buttonPrimary text-base font-semibold tracking-tight rounded-bl-full rounded-tl-full rounded-br-full xl:px-14 2xl:px-20 xl:py-6  mt-8 2xl:mt-8 px-12 md:py-5 py-6">
+        <button className="relative z-20 px-12 py-6 mt-8 text-base font-semibold tracking-tight text-white rounded-tl-full rounded-bl-full rounded-br-full 2xl:text-lg bg-buttonPrimary xl:px-14 2xl:px-20 xl:py-6 2xl:mt-8 md:py-5">
           <Link to="login"> Try it for free now! </Link>
         </button>
-        <span className="mt-5 mb-4 md:mb-10 lg:mb-20  text-textPrimaryColor">
+        <span className="mt-5 mb-4 md:mb-10 lg:mb-20 text-textPrimaryColor">
           No credit card required
         </span>
-        <div className="-mb-16 md:-mb-32 lg:-mb-56 px-6 overflow-hidden relative">
+        <div className="relative px-6 -mb-16 overflow-hidden md:-mb-32 lg:-mb-56">
           <img
-            src="/assets/qwikkerasset1.png "
+            src="/assets/mobilegif.gif "
             className="relative z-10"
             alt="demo"
           />
           <img
-            className="absolute top-28 w-56 -right-3"
+            className="absolute w-56 top-28 -right-3"
             src="/assets/qwikkerasset17.png"
             alt="altdd"
           />
@@ -106,11 +106,11 @@ export default function Home() {
       {/* second section */}
       <div
         style={{ background: "#fafbfc" }}
-        className="pt-40 md:pt-64 lg:pt-96 md:px-32 lg:px-0  "
+        className="pt-40 md:pt-64 lg:pt-96 md:px-32 lg:px-0 "
       >
         <h1
           style={{ letterSpacing: "-0.2rem" }}
-          className="px-5 text-center text-4xl md:text-5xl lg:text-5xl xl:text-6xl text-textPrimaryColor font-semibold"
+          className="px-5 text-4xl font-semibold text-center md:text-5xl lg:text-5xl xl:text-6xl text-textPrimaryColor"
         >
           The future of retail is in your hands
         </h1>
@@ -123,17 +123,17 @@ export default function Home() {
           {data.map((item, ind) => (
             <div
               key={ind}
-              className="flex flex-col mb-24 items-center mt-14 lg:mt-20"
+              className="flex flex-col items-center mb-24 mt-14 lg:mt-20"
             >
               <img
-                className=" w-72 lg:w-full  shadow-lg card-shadow  rounded-3xl "
+                className="shadow-lg w-72 lg:w-full card-shadow rounded-3xl"
                 src={item.img}
                 alt="dem"
               />
-              <h1 className="  text-customMediumFontMobile xl:text-2xl  tracking-tighter lg:px-0 px-6 lg:text-left  text-center text-textPrimaryColor font-semibold mt-6">
+              <h1 className="px-6 mt-6 font-semibold tracking-tighter text-center text-customMediumFontMobile xl:text-2xl lg:px-0 lg:text-left text-textPrimaryColor">
                 {item.header}
               </h1>
-              <p className="px-5 lg:px-0 text-textPrimaryColor text-opacity-80 lg:text-left  text-center mt-3">
+              <p className="px-5 mt-3 text-center lg:px-0 text-textPrimaryColor text-opacity-80 lg:text-left">
                 {item.description}
               </p>
             </div>
@@ -144,7 +144,7 @@ export default function Home() {
 
       {/* third section */}
 
-      <div className="flex flex-col items-center mt-36 lg:mt-20 mb-32 lg:grid lg:grid-cols-2 lg:gap-10 lg:px-14">
+      <div className="flex flex-col items-center mb-32 mt-36 lg:mt-20 lg:grid lg:grid-cols-2 lg:gap-10 lg:px-14">
         <img
           className="w-80 md:w-3/5 lg:w-3/4 card-shadow rounded-2xl"
           src="/assets/qwikkerasset5.png"
@@ -153,16 +153,16 @@ export default function Home() {
         <div className="px-6 md:px-20 lg:px-0">
           <h1
             style={{ letterSpacing: "-0.2rem" }}
-            className="text-4xl md:text-5xl mt-14 md:w-3/4 lg:w-full   text-textPrimaryColor   font-bold"
+            className="text-4xl font-bold md:text-5xl mt-14 md:w-3/4 lg:w-full text-textPrimaryColor"
           >
             Easy to build, easy to manage.
           </h1>
-          <p className="my-7 border-b pb-5 border-dashed md:text-lg text-textPrimaryColor text-opacity-80">
+          <p className="pb-5 border-b border-dashed my-7 md:text-lg text-textPrimaryColor text-opacity-80">
             Weblink helps you grow your online business without any hassle. get
             a powerful set of tools to create and monitor stunning pages for
             your customers
           </p>
-          <p className="my-7 italic text-textPrimaryColor text-opacity-80">
+          <p className="italic my-7 text-textPrimaryColor text-opacity-80">
             "Weblinks helped me build a clean and proffesional website with zero
             troubles. highly recommend."
           </p>
@@ -170,35 +170,35 @@ export default function Home() {
             Sam Stephan, Product Designer at Leesa
           </span>
           <img
-            className="w-28 mt-3"
+            className="mt-3 w-28"
             src="/assets/qwikkerasset8.svg"
             alt="logo"
           />
         </div>
       </div>
       {/* third section */}
-      <div className="px-4 py-24 mx-5 md:mx-16 rounded-3xl relative overflow-hidden    bg-bgAccent">
-        <div className="lg:grid lg:grid-cols-2 w-11/12 lg:m-auto  relative  ">
+      <div className="relative px-4 py-24 mx-5 overflow-hidden md:mx-16 rounded-3xl bg-bgAccent">
+        <div className="relative w-11/12 lg:grid lg:grid-cols-2 lg:m-auto ">
           <img
-            className="absolute w-48 left-0 top-6"
+            className="absolute left-0 w-48 top-6"
             src="/assets/qwikkerasset9.png"
             alt="asset"
           />
           <img
-            className="absolute w-48 right-0 -bottom-14"
+            className="absolute right-0 w-48 -bottom-14"
             src="/assets/qwikkerasset13.png"
             alt="asset"
           />
           <h1
             style={{ letterSpacing: "-0.2rem" }}
-            className="text-3xl relative z-10 md:text-5xl lg:text-6xl lg:px-0  text-textPrimaryColor   font-bold"
+            className="relative z-10 text-3xl font-bold md:text-5xl lg:text-6xl lg:px-0 text-textPrimaryColor"
           >
             Why to choose weblinks ?
           </h1>
-          <p className="relative z-10 lg:hidden pt-3 md:text-lg text-textPrimaryColor text-opacity-80 font-semibold tracking-tight">
+          <p className="relative z-10 pt-3 font-semibold tracking-tight lg:hidden md:text-lg text-textPrimaryColor text-opacity-80">
             East question, answers below-
           </p>
-          <div className="mt-24 lg:mt-0 md:px-20 lg:px-0 lg:flex lg:flex-col   ">
+          <div className="mt-24 lg:mt-0 md:px-20 lg:px-0 lg:flex lg:flex-col ">
             {data2.map((item, index) => (
               <div
                 key={index}
@@ -208,10 +208,10 @@ export default function Home() {
                     : "lg:static lg:mb-10 "
                 } mt-14 lg:px-0 lg:mt-0`}
               >
-                <figure className="bg-white rounded-bl-3xl rounded-tl-3xl rounded-br-3xl relative z-10  inline-block p-4">
+                <figure className="relative z-10 inline-block p-4 bg-white rounded-bl-3xl rounded-tl-3xl rounded-br-3xl">
                   <img className="w-10 md:w-14" src={item.img} alt="asset" />
                 </figure>
-                <h1 className="text-customMediumFontMobile md:text-3xl tracking-tighter text-textPrimaryColor py-2 font-bold">
+                <h1 className="py-2 font-bold tracking-tighter text-customMediumFontMobile md:text-3xl text-textPrimaryColor">
                   {item.title}
                 </h1>
                 <p
@@ -230,16 +230,16 @@ export default function Home() {
       <div className="px-6 lg:px-0 md:px-20 mt-28 lg:mt-40 lg:flex lg:flex-col lg:items-center lg:justify-center">
         <h1
           style={{ letterSpacing: "-0.2rem" }}
-          className="text-3xl md:text-5xl lg:text-6xl text-center px-10  lg:w-3/5 lg:m-auto xl:w-full  text-textPrimaryColor   font-bold"
+          className="px-10 text-3xl font-bold text-center md:text-5xl lg:text-6xl lg:w-3/5 lg:m-auto xl:w-full text-textPrimaryColor"
         >
           Build your websites your way.
         </h1>
-        <p className="text-center lg:px-10 lg:text-lg lg:w-3/5 xl:text-xl lg:m-auto lg:mt-6 mt-6 text-textPrimaryColor opacity-80">
+        <p className="mt-6 text-center lg:px-10 lg:text-lg lg:w-3/5 xl:text-xl lg:m-auto lg:mt-6 text-textPrimaryColor opacity-80">
           Create engaging, fast and smooth ecommerce pages your customers will
           rave about. Customize your site with a powerful suite of tools
         </p>
 
-        <div className="md:px-28 lg:w-10/12 lg:m-auto    lg:px-0  lg:space-x-12 lg:flex lg:justify-center">
+        <div className="md:px-28 lg:w-10/12 lg:m-auto lg:px-0 lg:space-x-12 lg:flex lg:justify-center">
           {data3.map((item, index) => (
             <div
               key={index}
@@ -253,13 +253,13 @@ export default function Home() {
               <h1 style={{ color: "#4bc2f1" }} className="mt-8">
                 {item.title}
               </h1>
-              <p className="text-textPrimaryColor lg:text-xl  my-4">
+              <p className="my-4 text-textPrimaryColor lg:text-xl">
                 {item.description}
               </p>
 
               <img className="mb-8 " src={item.logo} alt="logo" />
               <div className="flex items-center">
-                <a className="text-textPrimaryColor text-lg" href="#div">
+                <a className="text-lg text-textPrimaryColor" href="#div">
                   {item.link}
                 </a>
                 <div className="ml-2 text-textPrimaryColor">
